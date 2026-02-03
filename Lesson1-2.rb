5882e918f779364d9c864736aab9db5c3869990d
kaito-tsuchi <tsuchido-kaito-vw@ynu.jp>
2026-02-03 16:35:54 +0900

    単純計算、文字列の表示、オブジェクトと変数など、Rubyの基本の勉強

---
+++ Lesson1-2.rb
@@ -0,0 +1,7 @@
+puts "Hello,Woeld"
+puts "こんにちは"
+puts "Hello" + "world"
+puts 2+3
+puts "2" + "3"
+puts 3.to_s + "3"
+puts 3 + "3".to_i
---
+++ Lesson1-3.rb
@@ -0,0 +1,17 @@
+order = "カフェラテ"
+puts order
+puts "ご注文は" + order + "ですね？"
+puts order + "、オーダー入ります"
+
+puts "1+2の答えは#{1+2}です"
+puts "ご注文は#{order}ですね？"
+puts "ご注文は" + order + "ですね？"
+
+x = gets
+y = gets 
+puts x + y
+
+a = gets
+b = gets
+puts x.to_i + y.to_i
+
