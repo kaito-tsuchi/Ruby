93e943d7f6fd0236a45c00a109832432dd7fd13c
kaito-tsuchi <tsuchido-kaito-vw@ynu.jp>
2026-02-08 01:41:22 +0900

    配列

---
+++ Lesson3-2.rb
@@ -0,0 +1,16 @@
+##要素を取得する
+drinks = ["コーヒー","カフェラテ"]
+puts drinks[0]
+puts drinks[1]
+
+drinks = ["コーヒー","カフェラテ"]
+puts drinks [-1]
+puts drinks [-2]
+
+drinks = ["コーヒー","カフェラテ"]
+puts drinks.first
+puts drinks.last
+
+drinks = ["コーヒー","カフェラテ"]
+p drinks[2]
+
